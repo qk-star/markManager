7017b90b3074ca0a00eae00a2d302093d42e9914
vfnol <2402332409@qq.com>
2021-04-15 14:16:32 +0800

    新建登录分支新建登录组件配置路由

---
+++ mallmanager/src/components/login/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>login</div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+</style>
--- mallmanager/src/router/index.js
+++ mallmanager/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login/login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: Login,
+      path: '/login',
+      component: Login
     }
   ]
 })
