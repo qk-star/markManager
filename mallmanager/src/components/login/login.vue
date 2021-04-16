2bf2c9461ad5caedaf8fc31710a37c6d5f9a502c
vfnol <2402332409@qq.com>
2021-04-16 14:43:29 +0800

    登录功能，引入提示框

--- mallmanager/src/components/login/login.vue
+++ mallmanager/src/components/login/login.vue
@@ -1,12 +1,74 @@
 <template>
-  <div>login</div>
+  <div class="login-wrap">
+    <el-form label-position="top"
+             label-width="80px"
+             :model="formdata"
+             class="login-form">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"
+                   @click.prevent="handleLogin"
+                   class="login-button">登录</el-button>
+      </el-form-item>
+    </el-form>
+
+  </div>
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post('login', this.formdata).then(res => {
+        const { meta: { msg, status } } = res.data
+        // 登录成功
+        if (status === 200) {
+          // 跳转home
+          setTimeout(() => {
+            this.$router.push({ name: 'home' })
+          }, 2000)
+          // 提示成功
+          this.$message.success(msg)
+        } else {
+          // 不成功
+          // 1.提示消息
+          this.$message.error(msg)
+        }
+      })
+    }
   }
 }
 </script>
+
 <style scoped>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrap .login-form {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
+}
+.login-form .login-button {
+  width: 100%;
+}
 </style>
---
+++ mallmanager/src/plugins/http.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const MyHttpServer = {}
+MyHttpServer.install = function (Vue) {
+  axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+  // 4. 添加实例方法
+  Vue.prototype.$http = axios
+}
+
+export default MyHttpServer
